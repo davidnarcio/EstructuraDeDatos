ca32e863aed62fba3cf10a7825701ab21e7043af
davidnarcio <davidnarcio@gmail.com>
2026-09-03 21:38:51 -0700

    Add files via upload

---
+++ c#/InsercionFinal/InsercionFinal/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class InsercionFinal
+{
+    static void Main()
+    {
+        int[] arreglo = { 11, 21, 31, 41, 51, 61 };
+        int[] nuevoArreglo = new int[arreglo.Length + 1];
+
+        Console.WriteLine("---Insercion al final---");
+        Console.Write("El arreglo es: ");
+        for (int i = 0; i < arreglo.Length; i++)
+        {
+            Console.Write(arreglo[i] + " ");
+        }
+
+        Console.Write("\nQue numero vas a agregar? ");
+        int numInsertar = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < arreglo.Length; i++)
+        {
+            nuevoArreglo[i] = arreglo[i];
+        }
+        nuevoArreglo[arreglo.Length] = numInsertar;
+
+        Console.Write("El nuevo arreglo es: ");
+        for (int i = 0; i < nuevoArreglo.Length; i++)
+        {
+            Console.Write(nuevoArreglo[i] + " ");
+        }
+    }
+}
---
+++ c#/InsercionInicio/InsercionInicio/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class InsercionInicio
+{
+    static void Main()
+    {
+        int[] arreglo = { 11, 21, 31, 41, 51, 61 };
+        int[] nuevoArreglo = new int[arreglo.Length + 1];
+
+        Console.WriteLine("---Insercion al inicio---");
+        Console.Write("El arreglo es: ");
+        for (int i = 0; i < arreglo.Length; i++)
+        {
+            Console.Write(arreglo[i] + " ");
+        }
+
+        Console.Write("\nQue numero vas a agregar? ");
+        int numInsertar = int.Parse(Console.ReadLine());
+
+        nuevoArreglo[0] = numInsertar;
+        for (int i = 0; i < arreglo.Length; i++)
+        {
+            nuevoArreglo[i + 1] = arreglo[i];
+        }
+
+        Console.Write("El nuevo arreglo es: ");
+        for (int i = 0; i < nuevoArreglo.Length; i++)
+        {
+            Console.Write(nuevoArreglo[i] + " ");
+        }
+    }
+}
