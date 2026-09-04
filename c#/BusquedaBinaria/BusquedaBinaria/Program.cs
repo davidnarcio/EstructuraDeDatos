a4da5f744261f8f44a062f40baf89d92b170430b
davidnarcio <davidnarcio@gmail.com>
2026-09-03 21:36:04 -0700

    Add files via upload

---
+++ c#/BusquedaBinaria/BusquedaBinaria/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class BusquedaBinaria
+{
+    static void Main()
+    {
+        int[] arreglo = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        Console.WriteLine("---Busqueda binaria---");
+        Console.Write("El arreglo es: ");
+        for (int i = 0; i < arreglo.Length; i++)
+        {
+            Console.Write(arreglo[i] + " ");
+        }
+
+        Console.Write("\nQue numero quiere buscar? ");
+        int numeroBuscado = int.Parse(Console.ReadLine());
+
+        int inicio = 0;
+        int fin = arreglo.Length - 1;
+
+        while (inicio <= fin)
+        {
+            int medio = (inicio + fin) / 2;
+
+            if (arreglo[medio] == numeroBuscado)
+            {
+                Console.WriteLine("El numero esta en el lugar: " + (medio + 1));
+                return;
+            }
+            else if (numeroBuscado < arreglo[medio])
+            {
+                fin = medio - 1;
+            }
+            else
+            {
+                inicio = medio + 1;
+            }
+        }
+
+        Console.WriteLine("El numero no esta en el array");
+    }
+}
---
+++ c#/BusquedaSecuencial/BusquedaSecuencial/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class BusquedaSecuencial
+{
+    static void Main()
+    {
+        int[] arreglo = { 11, 21, 31, 41, 51, 61 };
+
+        Console.WriteLine("---Busqueda secuencial---");
+        Console.Write("El arreglo es: ");
+        for (int i = 0; i < arreglo.Length; i++)
+        {
+            Console.Write(arreglo[i] + " ");
+        }
+
+        Console.Write("\nQue numero quiere buscar? ");
+        int numeroBuscado = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < arreglo.Length; i++)
+        {
+            if (arreglo[i] == numeroBuscado)
+            {
+                Console.WriteLine("El numero esta en el lugar: " + (i + 1));
+                return;
+            }
+        }
+
+        Console.WriteLine("El numero no esta en el array");
+    }
+}
