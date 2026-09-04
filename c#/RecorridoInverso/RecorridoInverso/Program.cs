e8780711c4d5263c1151be152e3725a75e628978
davidnarcio <davidnarcio@gmail.com>
2026-09-03 21:39:46 -0700

    Add files via upload

---
+++ c#/RecorridoInverso/RecorridoInverso/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class RecorridoInverso
+{
+    static void Main()
+    {
+        int[] arreglo = { 11, 21, 31, 41, 51, 61 };
+
+        Console.WriteLine("---Recorrido inverso---");
+        Console.Write("El arreglo al reves es: ");
+        for (int i = arreglo.Length - 1; i >= 0; i--)
+        {
+            Console.Write(arreglo[i] + " ");
+        }
+    }
+}
---
+++ c#/recorridosecuencial/recorridosecuencial/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class RecorridoSecuencial
+{
+    static void Main()
+    {
+        int[] arreglo = { 11, 21, 31, 41, 51, 61 };
+
+        Console.WriteLine("---Recorrido secuencial---");
+        Console.Write("El arreglo es: ");
+        for (int i = 0; i < arreglo.Length; i++)
+        {
+            Console.Write(arreglo[i] + " ");
+        }
+    }
+}
